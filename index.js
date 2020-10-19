323d5ce11fb16f626bcca10d498c6667430b5244
itsaymhh <68047389+aymhh@users.noreply.github.com>
2020-10-20 10:29:12 +1100

    Fixed DM responding and cleaned up more code

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ fs.readdir("./commands/", (err, files) => {
 
   let jsfile = files.filter(f => f.split(".").pop() === "js")
   if(jsfile.length <= 0){
-    console.log("Couldn't find commands folder.");
+    console.log("There are no .js files in this directory...");
     return;
   }
 
@@ -50,7 +50,7 @@ bot.on('message', message => {
   let args = messageArray.slice(1);
   let commandfile = bot.commands.get(cmd.slice(prefix.length));
   let noCommandEmbed = new Discord.MessageEmbed()
-  .setColor("FF6961")
+   .setColor("FF6961")
    .setTitle("error!")
    .setDescription("Command doesn't exist")
    .addField("Please do `!help` to see what commands you can do!", "*Keep in mind these are all cap sensitive!*")
@@ -58,6 +58,20 @@ bot.on('message', message => {
    .setFooter() 
    .setFooter(message.author.tag + " | Peace Keeper", message.author.displayAvatarURL({dynamic: true, size: 1024}))
   ;
+  let notHereEmbed = new Discord.MessageEmbed()
+   .setColor('FF6961')
+   .setTitle('error!')
+   .addField('Do not send me messages directly', "Only use me in <#750998349276250123>")
+   .setTimestamp()
+   .setFooter(message.author.tag + " | " + bot.user.username, message.author.displayAvatarURL({dynamic: true, size: 1024}))
+  ;
+
+  if(message.author.bot) return ;
+  if(message.channel.type === "dm") {
+    message.reply(notHereEmbed).then(message => message.delete({timeout: 7500}));
+    return ;
+  }
+
   if(message.content.startsWith(prefix) && !commandfile) {
     message.channel.send(noCommandEmbed).then(message => message.delete({timeout: 6000}));
     return;
@@ -96,7 +110,7 @@ bot.on('message', message => {
   .addField("Status:", member.presence.status, true)
   .addField("Bot?", member.user.bot, true)
   .setFooter(`Peace Keeper`)
-  .setColor('##007FBD')
+  .setColor('#007FBD')
   logChannel.send(leaveEmbed);
 });
 
@@ -123,18 +137,21 @@ bot.on('message', message => {
 
   // Logging messages that have been deleted
   bot.on('messageDelete', async message => {
-  await (`10`)
+  if(message.author.bot) return;
+  
   let deleteEmbed = new Discord.MessageEmbed()
-  .setTitle("**A message was deleted...**")
-  .setDescription("By: " + `<@${message.author.id}>`)
-  .setTimestamp()
-  .setThumbnail(message.author.displayAvatarURL({dynamic: true, size: 1024}))
-  .addField("Message:", '"**'+`${message.content}`+'**"' + " *in channel:*" + " " + `${message.channel}`, false)
-  .setFooter(`Peace Keeper`)
-  .setColor('#E3E3E3')
+   .setTitle("**A message was deleted...**")
+   .setDescription("By: " + `<@${message.author.id}>`)
+   .setTimestamp()
+   .setThumbnail(message.author.displayAvatarURL({dynamic: true, size: 1024}))
+   .addField("Message:", '"**'+`${message.content}`+'**"' + " *in channel:*" + " " + `${message.channel}`, false)
+   .setFooter(`Peace Keeper`)
+   .setColor('#E3E3E3')
+  ;
   
   let logChannel = message.guild.channels.cache.find(channel => channel.name === 'bot-peacekeeper-logger')
   if(!logChannel) return;
+  
   if(message.author.bot) return;
   logChannel.send(deleteEmbed);
 });
@@ -196,93 +213,6 @@ bot.on('message', (message) => {
     return;
   }
 });
-
-bot.on('message', async(message, args) => {
-  if(message.channel.type === "dm" && message.content === "openTicket") { 
-
-    hewiaGuild = bot.guilds.cache.get("725636740232249366");
-    const categoryID = "730413542653820958";
-    let servericon = hewiaGuild.iconURL({dynamic: true, size: 1024});
-    let StaffPing = '730420809642016828'
-    let ticketargs = args.slice(0).join(" ").split('|');    
-
-    // Error Embed due to no reason provided
-    var embedCreateNoReasonTicket = new Discord.MessageEmbed()
-     .setTitle("**error!**")
-     .setColor('#FF6961')
-     .setDescription("Please provide a reason on why you are opening this ticket.")
-     .addField("Format: ", "`openTicket <reason>`")
-     .setTimestamp()
-     .setFooter("Peace Keeper");
-    if(!ticketargs[0]) return message.channel.send(embedCreateNoReasonTicket).then(msg => msg.delete({timeout: 7000}));
-
-    // Embed for users trying to open a ticket in which they already have ticket open.
-    const newTicketErrEmbed = new Discord.MessageEmbed()
-     .setColor('FF6961')
-     .setTitle("**error!**")
-     .addField("You currently have a ticket open.", "Don't make a new ticket till the current one is closed.")
-     .setTimestamp()
-     .setFooter(message.author.tag + " | Peace Keeper", message.author.displayAvatarURL({dynamic: true, size: 1024}))
-    if(hewiaGuild.channels.cache.find(channel => channel.name === "ticket-" + message.author.username)) return message.channel.send(newTicketErrEmbed);
-
-    hewiaGuild.channels.create("ticket - " + message.author.username, {reason: 'Private ticket channel for ' + message.author.username}).then((createdChan) => {
-        createdChan.setParent(categoryID).then((settedParent) => {
-            settedParent.updateOverwrite(message.author, {
-              "VIEW_CHANNEL": true, 
-              "SEND_MESSAGES": true, 
-              "ATTACH_FILES": true,}).then(createdChan.setTopic(`${message.author.id}`));
-            
-   // Embed for confirming the ticket is open and it's location.
-   var embedCreateTicket = new Discord.MessageEmbed()
-   .setTitle("**Tickets**")
-   .setColor('#ABDFF2')
-   .setThumbnail(servericon)
-   .setDescription("A ticket has been opened!")
-   .addField("Ticket Location:", createdChan, true)
-   .addField("Ticket Reason:", ticketargs[0] , true)
-   .setTimestamp()
-   .setFooter("Peace Keeper");
-   message.channel.send(embedCreateTicket)
-  ;
-   // Embed that gets send when the ticket channel gets open and informs staff and the ticket author about this ticket.
-   var embedParent = new Discord.MessageEmbed()
-    .setTitle("**Tickets**")
-    .setColor('#ABDFF2')
-    .setDescription("All online staff have been notified, we will get back to you as soon as possible.")
-    .addField("Ticket Name:", "#ticket-" + message.author.username, true)
-    .addField("Opened By:", `<@${message.author.id}>`, true)
-    .addField("Ticket Reason:", ticketReason , true)
-    .addField("__Be warned!__", "We do not take harrassment lightly, if you act malicous in any form towards staff, the ticket will be closed and you will be punished.")
-    .addField("Note:", "*To get this chat's transcript, please allow direct messages from this server.\nIf unsure on how to turn this on, ask staff here on how to do this.*")
-    .setTimestamp()
-    .setFooter("Peace Keeper")
-   ;
-   settedParent.send(`<@&${StaffPing}>`)
-   settedParent.send(`<@${message.author.id}>`)
-   settedParent.send(embedParent);
-   createdChan.setTopic(`${message.author.id}`)
-        });
-    });
-
-    var embedOpenTicket = new Discord.MessageEmbed()
-     .setTitle("**Tickets**")
-     .setColor('#ABDFF2')
-     .setThumbnail(servericon)
-     .setDescription("A ticket has been opened!")
-     .addField("Ticket Name:", "#ticket-" + message.author.username, true)
-     .addField("Opened By:", `<@${message.author.id}>`, true)
-     .addField("Ticket Reason:", ticketReason , true)
-     .setTimestamp()
-     .setFooter("Peace Keeper");
-    ;
-    var logChannel = hewiaGuild.channels.cache.find(channel => channel.name === "ticket-logs");
-
-    message.delete().then(() => {
-    logChannel.send(embedOpenTicket);
-    });
-  }
-  return ;
-})
 
 // Confirming the bot is running and is changing the status of it on discord
 bot.on('ready', () => {
